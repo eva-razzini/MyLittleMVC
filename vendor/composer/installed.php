d767c617183051f943116bf46211cbbdf097e0f4
unknown <eva.razzini@laplateforme.com>
2024-03-10 22:43:20 +0100

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nico-rvl/revisions-poo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ed9395a8552929a8e8f2ed81ac0387b502e69b1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nico-rvl/revisions-poo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ed9395a8552929a8e8f2ed81ac0387b502e69b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
